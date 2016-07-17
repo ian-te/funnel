14177af8e9eded8aafe3ef23be99db5cf1f46285
yan.te <yan.te@team.wrike.com>
2016-07-17 19:18:59 +0300

    added data and d3 handling

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,10 +6,44 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController() {
+  function MainController(d3, $timeout) {
     var vm = this;
+    vm.awesomeThings = 'TEST';
 
-    vm.awesomeThings = [];
+    d3.csv("/assets/data/downloads.csv",function(d) {
+      var tmp = d.date.split('.');
+      var date = '2016-' + tmp[1] + '-' + tmp [0];
+
+
+      return {
+        date : new Date(date),
+        product: d.product,
+        country: d.country,
+        os: d.os,
+        count: +d.count
+      };
+    }, function(data) {
+      var sum = d3.sum(data, function(d) { return d.count; });
+
+      d3.select("#countries").selectAll("option")
+          .data(d3.map(data, function(d){return d.country;}).keys())
+          .enter()
+          .append("option")
+          .text(function(d){return d;})
+          .attr("value",function(d){return d;});
+
+      d3.select("#products").selectAll("option")
+          .data(d3.map(data, function(d){return d.product;}).keys())
+          .enter()
+          .append("option")
+          .text(function(d){return d;})
+          .attr("value",function(d){return d;});
+
+      $timeout(function(){
+        vm.downloads = data;
+        vm.sum = sum;
+      });
+    });
 
   }
 })();
