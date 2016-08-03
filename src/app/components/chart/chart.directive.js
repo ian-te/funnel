20a840db9627bad339cae802623073635950a680
yan.te <yan.te@team.wrike.com>
2016-08-03 09:10:39 +0300

    progress

--- src/app/components/chart/chart.directive.js
+++ src/app/components/chart/chart.directive.js
@@ -27,7 +27,7 @@
             var width = 500;
             var height = 300;
             var barWidth = 100;
-            var p = d3.precisionPrefix(1e5, 1.3e6),
+            var p = d3.precisionPrefix(1e3, 1.3e6),
                 f = d3.formatPrefix("." + p, 1.3e6);
 
             var chart = d3.select($element[0]).select('svg')
@@ -37,13 +37,13 @@
 
             $scope.$watch(function(){
                 vm.render();
-                $log.log(vm.data)
+                // $log.log(vm.data)
             });
 
             vm.render = function(){
                 chart.selectAll('*').remove();
                 var y = d3.scaleLinear().range([height, 0]);
-                y.domain([0, d3.max(vm.data, function(d) { return d.value; })]);
+                y.domain([0, d3.max(vm.data, function(d) { return d.value.total; })]);
 
 
                 var bar = chart.selectAll('g')
@@ -54,13 +54,14 @@
 
                 bar.append('rect')
                     .attr('width', barWidth - 6)
-                    .attr('y', function(d){ return y(d.value) })
-                    .attr('height', function(d){ return height - y(d.value) });
+                    .attr('y', function(d){ return y(d.value.total) })
+                    .attr('height', function(d){ return height - y(d.value.total) });
+
                 bar.append('text')
                     .attr("x", barWidth-20 / 2)
-                    .attr("y", function(d) { return y(d.value) + 3; })
+                    .attr("y", function(d) { return y(d.value.total) + 3; })
                     .attr("dy", ".75em")
-                    .text(function(d) { return d.key + ' ' + f(d.value); });
+                    .text(function(d) { return d.key + ' ' + f(d.value.total); });
             }
 
         }
--- src/app/test/test.controller.js
+++ src/app/test/test.controller.js
@@ -6,29 +6,96 @@
         .controller('TestController', TestController);
 
     /** @ngInject */
-    function TestController(backend, d3) {
+    function TestController(backend, d3, $scope) {
         var vm = this;
         vm.filters = {};
         vm.click = function(){
 
         };
         backend.get().then(function (d) {
-            vm.data = prepareChartData(d);
-
             vm.dataHasLoaded = true;
+            vm.fullData = d;
+            vm.data = prepareChartData(vm.fullData);
+            vm.filters = getAvailableFilters(vm.fullData);
         });
+        function getAvailableFilters(d){
+            var data =  {
+                product: d3.map(d, function(d){return d.product;}).keys(),
+                date: d3.map(d, function(d){return d.date;}).keys(),
+                country: d3.map(d, function(d){return d.country;}).keys()
+            };
+            return data;
+        }
+        $scope.$watchCollection('test.selectedFilters', function(){
+            if(vm.fullData){
+                vm.data = prepareChartData(vm.fullData, vm.selectedFilters);
+            }
+        });
+        function prepareChartData(d, filter){
+            if(filter){
+                for(var i in filter){
+                    if(filter[i].length > 0 && filter[i].indexOf('All') == -1){
+                        d = d.filter(function(item) {
+                            return filter[i].indexOf(item[i]) > -1
+                        })
+                    }
+                    if(typeof filter[i] === 'object'){
+                        d = d.filter(function(item) {
+                            if(i != item.action) {
+                                return true;
+                            } else {
+                                for (var k in filter[i]) {
+                                    return item[k] == filter[i][k]
+                                }
+                            }
+                        })
 
-        function prepareChartData(d){
-            d = d.filter(function(d){ return d.country == 'China'});
+                    }
+                }
+            }
+
+            vm.channelFilters = d3.nest()
+                .key(function(d) {return d.action })
+                .rollup(function(d){
+                    var filter = {};
+                    d.forEach(function(item, k){
+                        var excluded = ['action','source','country','count','date','product'];
+                        for(var i in item) {
+                            if(!filter[i]) filter[i] = [];
+                            if(filter[i].indexOf(item[i]) == -1 && excluded.indexOf(i) == -1 ){
+                                filter[i].push(item[i])
+                            }
+                            if(filter[i].length == 0){delete filter[i]}
+                        }
+                    });
+                    return filter
+                })
+                .entries(d);
+
             var data = d3.nest()
                 .key(function(d) {return d.action })
                 .rollup(function(d){
-                    return d3.sum(d, function(g) {
-                        return Math.round(g.count);
-                    })
+                    return {
+                        total: d3.sum(d, function (g) {
+                            return Math.round(g.count);
+                        }),
+                        filters: function(){
+                            var filter = {};
+                            d.forEach(function(item, k){
+                                var excluded = ['action','source','country','count','date','product'];
+                                for(var i in item) {
+                                    if(!filter[i]) filter[i] = [];
+                                    if(filter[i].indexOf(item[i]) == -1 && excluded.indexOf(i) == -1 ){
+                                        filter[i].push(item[i])
+                                    }
+                                    if(filter[i].length == 0){delete filter[i]}
+                                }
+                            });
+                            return filter;
+                        }()
+                    }
                 })
                 .entries(d);
-            
             return data;
         }
 
